fc4ec29c2512e3fb8e06534216f5a6c6ee267137
Houssam <houssam.naous1@gmail.com>
2021-02-13 15:04:03 +0100

    added moon and sun

--- planets/ViewController.swift
+++ planets/ViewController.swift
@@ -24,24 +24,52 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        let earth = SCNNode()
-        earth.geometry = SCNSphere(radius: 0.2)
-        // earth.geometry?.firstMaterial?.diffuse.contents
+        createScene()
+    }
+    
+    // Create a Scene with the Sun, Earth, and moon spinning around each other.
+    func createScene() {
+
+        // Create Sun that spins around itself
+        let sun = constructPlanet(geometry: SCNSphere(radius: 0.3), position: SCNVector3(0,0,-2), diffuse: UIImage(named: "Sun"), specular: nil, emission: nil, normal: nil)
+        let sunAction = spinForeverAction(durationSecs: 10)
+        sun.runAction(sunAction)
         
-        earth.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "EarthDay")
-        earth.geometry?.firstMaterial?.specular.contents = UIImage(named: "EarthSpecular")
-        earth.geometry?.firstMaterial?.emission.contents = UIImage(named: "EarthClouds")
-        earth.geometry?.firstMaterial?.normal.contents = UIImage(named: "EarthNormal")
-        earth.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(sun)
         
+        // Add the earth and make it spin around the sun
+        let earth = constructPlanet(geometry: SCNSphere(radius: 0.1), position: SCNVector3(0,0,-1), diffuse: UIImage(named: "EarthDay"), specular: UIImage(named: "EarthSpecular"), emission: UIImage(named: "EarthClouds"), normal: UIImage(named: "EarthNormal"))
+
         self.sceneView.scene.rootNode.addChildNode(earth)
+        let earthSpin = spinForeverAction(durationSecs: 4)
+        earth.runAction(earthSpin) // run rotation on the earth
+                      
+        sun.addChildNode(earth)
         
-        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
-        let forever  = SCNAction.repeatForever(action)
-        earth.runAction(forever) // run rotation on the earth
+        // Add the Moon
+        let moon = constructPlanet(geometry: SCNSphere(radius: 0.05), position: SCNVector3(0.2, 0, 0), diffuse: UIImage(named: "Moon"), specular: nil, emission: nil, normal: nil)
+        let moonSpin = spinForeverAction(durationSecs: 5)
+        moon.runAction(moonSpin)
+        earth.addChildNode(moon)
         
     }
+    
+    func constructPlanet(geometry : SCNGeometry, position: SCNVector3, diffuse: UIImage?, specular: UIImage?, emission: UIImage?, normal: UIImage?) -> SCNNode {
+        let planet = SCNNode(geometry: geometry)
+        planet.geometry?.firstMaterial?.diffuse.contents = diffuse
+        planet.geometry?.firstMaterial?.specular.contents = specular
+        planet.geometry?.firstMaterial?.emission.contents = emission
+        planet.geometry?.firstMaterial?.normal.contents = normal
+        planet.position = position
+        
+        return planet
+    }
 
+    func spinForeverAction(durationSecs: TimeInterval) -> SCNAction {
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: durationSecs)
+        let forever  = SCNAction.repeatForever(action)
+        return forever
+    }
 
 }
 
