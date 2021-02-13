09a07340ba5e675d290de6d5c46a94d8c4e040a8
Houssam <houssam.naous1@gmail.com>
2021-02-13 11:17:30 +0100

    added earth globe over scene

--- planets/ViewController.swift
+++ planets/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: 0.2)
+        // earth.geometry?.firstMaterial?.diffuse.contents
+        
+        earth.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "EarthDay")
+        earth.position = SCNVector3(0,0,-1)
+        
+        self.sceneView.scene.rootNode.addChildNode(earth)
+        
     }
 
 
